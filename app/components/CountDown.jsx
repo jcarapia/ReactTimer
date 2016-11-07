c744bc789ab3dc06da0cd61efed682392994f73a
Jorge <jcarapia83@Gmail.com>
2016-11-06 23:09:58 -0800

    initial commit

---
+++ app/components/CountDown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var CountDown = React.createClass({
+	render: function(){
+		return (
+			<p> this is the CountDown </p>	
+		);
+	}
+});
+
+module.exports = CountDown;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'script!jquery/dist/jquery.min.js',
+		'script!foundation-sites/dist/foundation.min.js',
+		'./app/app.jsx'
+	],
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js'	
+	},
+	externals: {
+		jquery: 'jQuery'
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'$': 'jquery',
+			'jQuery': 'jquery'
+		})
+	], 
+	resolve:{
+		root: __dirname,
+		alias: {
+			Main: 'app/components/Main.jsx',
+			Nav: 'app/components/Nav.jsx',
+			CountDown: 'app/components/CountDown.jsx',
+			applicationStyles: 'app/styles/app.scss'
+		},
+		extensions: ['', '.js', '.jsx']
+	}, 
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015', 'stage-0']
+				},
+				test: /\.jsx?$/,
+				exclude: /(node_modules | bower.components)/
+			}
+		]
+	},
+	devtool: 'eval-source-map'
+};
